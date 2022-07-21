095cd7411390fee60b12e8574d1bfa80270c8ac4
GGuppy <kylanplunk@gmail.com>
2022-07-20 19:09:32 -0500

    Potentially fixed Citizens compatibility?

--- src/main/java/net/gguppy/itemdeath/ItemDeath.java
+++ src/main/java/net/gguppy/itemdeath/ItemDeath.java
@@ -23,88 +23,92 @@ public final class  ItemDeath extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        event.setKeepInventory(true);
-        event.getDrops().clear();
-
         Player eventPlayer = event.getEntity().getPlayer();
-        Inventory inv = eventPlayer.getInventory();
-        //Generate a random number between 1-5 to determine the amount of item slots
-        //to drop.
-        int randomItemDrop = ThreadLocalRandom.current().nextInt(1, 5 + 1);
-        //eventPlayer.sendMessage("You dropped " + randomItemDrop + " item slots.");
-        //Generate a number between 5-40% to determine the amount of
-        //experience to drop.
-        int randomExpDrop = ThreadLocalRandom.current().nextInt(5, 40 + 1);
-
-        ItemStack[] contents = inv.getContents();
-
-        //This will hold the string of the item names for sending to the player later.
-        LinkedList<String> itemNames = new LinkedList<>();
-        //Similarly, this will hold the number of items of each type.
-        LinkedList<Integer> itemCounts = new LinkedList<>();
+        if(!eventPlayer.hasMetadata("NPC")){
+            event.setKeepInventory(true);
+            event.getDrops().clear();
 
 
-        //Loops randomItemDrop amount of times, randomItemDrop being the number of items randomly generated
-        //to be dropped.
-        for(int i = 0; i < randomItemDrop && i >= 0; i++) {
+            Inventory inv = eventPlayer.getInventory();
+            //Generate a random number between 1-5 to determine the amount of item slots
+            //to drop.
+            int randomItemDrop = ThreadLocalRandom.current().nextInt(1, 5 + 1);
+            //eventPlayer.sendMessage("You dropped " + randomItemDrop + " item slots.");
+            //Generate a number between 5-40% to determine the amount of
+            //experience to drop.
+            int randomExpDrop = ThreadLocalRandom.current().nextInt(5, 40 + 1);
 
-            //This gets the index of a random item in the inventory.
-            int randomItem = (int) (Math.random() * contents.length);
-            //This checks if that item is air. If not, it'll continue. If so, it'll
-            //check another item slot.
-            if(contents[randomItem] != null) {
-                //This adds the item to the list of item that will be dropped.
-                event.getDrops().add(contents[randomItem]);
-                //This stores the itemName for later.
-                String itemName = contents[randomItem].getType().toString();
-                //If it has a custom name, use that instead.
-                if(contents[randomItem].getItemMeta().hasDisplayName()){
-                    itemName = contents[randomItem].getItemMeta().getDisplayName();
+            ItemStack[] contents = inv.getContents();
+
+            //This will hold the string of the item names for sending to the player later.
+            LinkedList<String> itemNames = new LinkedList<>();
+            //Similarly, this will hold the number of items of each type.
+            LinkedList<Integer> itemCounts = new LinkedList<>();
+
+
+            //Loops randomItemDrop amount of times, randomItemDrop being the number of items randomly generated
+            //to be dropped.
+            for(int i = 0; i < randomItemDrop && i >= 0; i++) {
+
+                //This gets the index of a random item in the inventory.
+                int randomItem = (int) (Math.random() * contents.length);
+                //This checks if that item is air. If not, it'll continue. If so, it'll
+                //check another item slot.
+                if(contents[randomItem] != null) {
+                    //This adds the item to the list of item that will be dropped.
+                    event.getDrops().add(contents[randomItem]);
+                    //This stores the itemName for later.
+                    String itemName = contents[randomItem].getType().toString();
+                    //If it has a custom name, use that instead.
+                    if(contents[randomItem].getItemMeta().hasDisplayName()){
+                        itemName = contents[randomItem].getItemMeta().getDisplayName();
+                    }
+                    //Below code changes name formats from "IRON_AXE" to "Iron Axe".
+                    itemName = itemName.replace("_", " ");
+                    itemName = WordUtils.capitalizeFully(itemName);
+                    //Stores the itemName inside the list of itemNames.
+                    itemNames.add(itemName);
+                    //Similar for the itemCount.
+                    int itemCount = contents[randomItem].getAmount();
+                    itemCounts.add(itemCount);
+                    //This removes the item from the inventory (since we are dropping it).
+                    contents[randomItem] = null;
+                    //Updates the inventory with the new contents.
+                    eventPlayer.getInventory().setContents(contents);
+                } else {
+                    //This runs if the item is air, allowing the loop to continue to
+                    //search for a valid item to drop.
+                    i--;
                 }
-                //Below code changes name formats from "IRON_AXE" to "Iron Axe".
-                itemName = itemName.replace("_", " ");
-                itemName = WordUtils.capitalizeFully(itemName);
-                //Stores the itemName inside the list of itemNames.
-                itemNames.add(itemName);
-                //Similar for the itemCount.
-                int itemCount = contents[randomItem].getAmount();
-                itemCounts.add(itemCount);
-                //This removes the item from the inventory (since we are dropping it).
-                contents[randomItem] = null;
-                //Updates the inventory with the new contents.
-                eventPlayer.getInventory().setContents(contents);
-            } else {
-                //This runs if the item is air, allowing the loop to continue to
-                //search for a valid item to drop.
-                i--;
             }
+
+            //Sends a message to the player containing all the items they dropped with amounts.
+            String messageToSend = "You dropped: ";
+            for(int i = 0; i < itemNames.size(); i++) {
+                messageToSend += itemNames.get(i) + " x" + itemCounts.get(i) + ", ";
+            }
+            int lastCommaIndex = messageToSend.lastIndexOf(",");
+            messageToSend = messageToSend.substring(0, lastCommaIndex) + messageToSend.substring(lastCommaIndex + 1);
+            eventPlayer.sendMessage(messageToSend);
+
+            //Get player current level
+            int level = eventPlayer.getLevel();
+            //Store that level
+            int cachedLevel = level;
+            //Calculate the new level. It is equal to level - randomXP dropped.
+            level = (int) (level - (level * (randomExpDrop / 100.0)));
+
+            //Now we need to calculate the amount of XP to drop on the ground.
+            //It is equal to their old total XP minus the new level's total XP.
+            //That number is then multiplied by 0.25 as requested so effectively 75%
+            //of xp that would've been dropped is lost permanently.
+            float oldXP = levelToExp(cachedLevel);
+            float newXP = levelToExp(level);
+            event.setNewLevel(level);
+            //0.25 is the modifier chosen by person who commissioned the plugin.
+            event.setDroppedExp((int) ((oldXP - newXP + 1) * 0.25));
         }
 
-        //Sends a message to the player containing all the items they droppped with amounts.
-        String messageToSend = "You dropped: ";
-        for(int i = 0; i < itemNames.size(); i++) {
-            messageToSend += itemNames.get(i) + " x" + itemCounts.get(i) + ", ";
-        }
-        int lastCommaIndex = messageToSend.lastIndexOf(",");
-        messageToSend = messageToSend.substring(0, lastCommaIndex) + messageToSend.substring(lastCommaIndex + 1);
-        eventPlayer.sendMessage(messageToSend);
-
-        //Get player current level
-        int level = eventPlayer.getLevel();
-        //Store that level
-        int cachedLevel = level;
-        //Calculate the new level. It is equal to level - randomXP dropped.
-        level = (int) (level - (level * (randomExpDrop / 100.0)));
-
-        //Now we need to calculate the amount of XP to drop on the ground.
-        //It is equal to their old total XP minus the new level's total XP.
-        //That number is then multiplied by 0.25 as requested so effectively 75%
-        //of xp that would've been dropped is lost permanently.
-        float oldXP = levelToExp(cachedLevel);
-        float newXP = levelToExp(level);
-        event.setNewLevel(level);
-        //0.25 is the modifier chosen by person who commissioned the plugin.
-        event.setDroppedExp((int) ((oldXP - newXP + 1) * 0.25));
     }
 
     private float levelToExp(int level) {
