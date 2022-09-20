f1650b603d789073ebf05e68093d5ff15d5d4f08
GGuppyGit <kylanplunk@gmail.com>
2022-09-20 13:42:27 -0500

    Rewritten to be less aggressive as far as handling items
    XP functionality not working

--- src/main/java/net/gguppy/itemdeath/ItemDeath.java
+++ src/main/java/net/gguppy/itemdeath/ItemDeath.java
@@ -3,7 +3,10 @@ package net.gguppy.itemdeath;
 
 import org.apache.commons.lang3.Range;
 import org.apache.commons.text.WordUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.entity.Player;
@@ -20,119 +23,110 @@ import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public final class  ItemDeath extends JavaPlugin implements Listener {
+    public FileConfiguration config = getConfig();
+    public static int itemDropPercent = 0;
+    public static boolean honorVanishingCurse = false;
+    public static boolean logDeathLoot = false;
 
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        setupConfigs();
+        itemDropPercent = Integer.parseInt(config.getString("percent-items-to-drop"));
+        getLogger().info("Players will drop " + itemDropPercent + "% of their items on death.");
+        honorVanishingCurse = Boolean.parseBoolean(config.getString("honor-vanish-curse"));
+        getLogger().info("Honor curse of vanishing: " + honorVanishingCurse);
+        logDeathLoot = Boolean.parseBoolean(config.getString("log-death-loot"));
+        getLogger().info("Log loot on death: " + logDeathLoot);
+
 
     }
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        Entity eventEntity = event.getEntity();
-
-        if (!eventEntity.hasMetadata("NPC")) {
+//        Entity eventEntity = event.getEntity();
+//
+//        if (!eventEntity.hasMetadata("NPC")) {
             Player eventPlayer = event.getEntity().getPlayer();
-
-            event.getDrops().clear();
-
-            ItemStack[] contents = eventPlayer.getInventory().getContents();
-            //Generate a random number between 1-5 to determine the amount of item slots
-            //to drop.
-            int randomItemsToDrop = ThreadLocalRandom.current().nextInt(1, 5 + 1);
-            //eventPlayer.sendMessage("You dropped " + randomItemsToDrop + " item slots.");
-            //Generate a number between 5-40% to determine the amount of
-            //experience to drop.
-            int randomExpDrop = ThreadLocalRandom.current().nextInt(5, 40 + 1);
-
-
-            //Will hold the names of the dropped items and their amounts.
-            LinkedHashMap<String, Integer> droppedItems = new LinkedHashMap<>();
-
-
-            //This will get the indexes of every non-empty item slot, allowing us to randomly choose from them.
-            LinkedList<Integer> nonAirItemSlots = new LinkedList<>();
-            for (int i = 0; i < contents.length; i++) {
-                if (contents[i] != null) {
-                    nonAirItemSlots.add(i);
-                }
-            }
-
-            if (randomItemsToDrop > nonAirItemSlots.size()) {
-                randomItemsToDrop = nonAirItemSlots.size();
-            }
-
-            int slotIndex = 0;
+//
+//            event.getDrops().clear();
+//
+//            ItemStack[] contents = eventPlayer.getInventory().getContents();
+//            //Generate a random number between 1-5 to determine the amount of item slots
+//            //to drop.
+//            int randomItemsToDrop = ThreadLocalRandom.current().nextInt(1, 5 + 1);
+//            //eventPlayer.sendMessage("You dropped " + randomItemsToDrop + " item slots.");
+//            //Generate a number between 5-40% to determine the amount of
+//            //experience to drop.
+//            int randomExpDrop = ThreadLocalRandom.current().nextInt(5, 40 + 1);
+//
+//
+//            //Will hold the names of the dropped items and their amounts.
+//            LinkedHashMap<String, Integer> droppedItems = new LinkedHashMap<>();
+//
+//
+//            //This will get the indexes of every non-empty item slot, allowing us to randomly choose from them.
+//            LinkedList<Integer> nonAirItemSlots = new LinkedList<>();
+//            for (int i = 0; i < contents.length; i++) {
+//                if (contents[i] != null) {
+//                    nonAirItemSlots.add(i);
+//                }
+//            }
+//
+//            if (randomItemsToDrop > nonAirItemSlots.size()) {
+//                randomItemsToDrop = nonAirItemSlots.size();
+//            }
+//
+//            int slotIndex = 0;
             Location deathLoc = eventPlayer.getLocation();
-            LinkedList droppedItemsList = new LinkedList();
-            //Randomly choose "randomItemsToDrop" amount of non-air item slots to drop.
-            for (int i = 0; i < randomItemsToDrop; i++) {
-                //Choose a random item slot.
-                int randomItemSlot = ThreadLocalRandom.current().nextInt(0, nonAirItemSlots.size());
-                slotIndex = nonAirItemSlots.get(randomItemSlot);
-                //Remove that item from contents
-
-                ItemStack currItem = contents[slotIndex];
-
-
-                //Also remember there could be duplicate items
-                if (droppedItems.get(currItem.getType().name()) == null) {
-                    droppedItems.put(currItem.getType().name(), currItem.getAmount());
-                } else {
-                    int t = currItem.getAmount() + droppedItems.get(currItem.getType().name());
-                    droppedItems.put(currItem.getType().name(), t);
-                }
-
-                DropItemsRunnable dropItemsRunnable = new DropItemsRunnable(eventPlayer, currItem, deathLoc);
-                contents[slotIndex] = null;
-                dropItemsRunnable.runTaskLater(this, 5L);
-                nonAirItemSlots.remove(randomItemSlot);
-
-
-            }
-            SetPlayerDataRunnable setPlayerDataRunnable = new SetPlayerDataRunnable(eventPlayer, contents, randomExpDrop, deathLoc);
+//            LinkedList droppedItemsList = new LinkedList();
+//            //Randomly choose "randomItemsToDrop" amount of non-air item slots to drop.
+//            for (int i = 0; i < randomItemsToDrop; i++) {
+//                //Choose a random item slot.
+//                int randomItemSlot = ThreadLocalRandom.current().nextInt(0, nonAirItemSlots.size());
+//                slotIndex = nonAirItemSlots.get(randomItemSlot);
+//                //Remove that item from contents
+//
+//                ItemStack currItem = contents[slotIndex];
+//
+//
+//                //Also remember there could be duplicate items
+//                if (droppedItems.get(currItem.getType().name()) == null) {
+//                    droppedItems.put(currItem.getType().name(), currItem.getAmount());
+//                } else {
+//                    int t = currItem.getAmount() + droppedItems.get(currItem.getType().name());
+//                    droppedItems.put(currItem.getType().name(), t);
+//                }
+//
+//                contents[slotIndex] = null;
+//
+//                nonAirItemSlots.remove(randomItemSlot);
+//
+//
+//            }
+            SetPlayerDataRunnable setPlayerDataRunnable = new SetPlayerDataRunnable(eventPlayer, deathLoc, itemDropPercent, honorVanishingCurse, logDeathLoot);
             setPlayerDataRunnable.runTaskLater(this, 20L);
-
-            double newTotalExperience = eventPlayer.getLevel();
-            newTotalExperience += eventPlayer.getExp();
-            newTotalExperience = levelToExp(newTotalExperience);
-            double oldTotalExperience = newTotalExperience;
-
-
-            //Calculate the new level. It is equal to level - randomXP dropped.
-            newTotalExperience = (newTotalExperience - (newTotalExperience * (randomExpDrop  / 100.0)));
-            Entity entity = deathLoc.getWorld().spawnEntity(deathLoc, org.bukkit.entity.EntityType.EXPERIENCE_ORB);
-            event.setDroppedExp(0);
-            ExperienceOrb expOrb = (ExperienceOrb) entity;
-            expOrb.setExperience((int) ((oldTotalExperience - newTotalExperience) * 0.25));
-            double orbNum = (oldTotalExperience - newTotalExperience) * 0.25;
-            expOrb.setExperience((int) orbNum);
-
-            eventPlayer.setLevel(0);
-            eventPlayer.setExp(0);
-            eventPlayer.giveExp((int)newTotalExperience);
-            event.setKeepLevel(true);
-            
-            String mes = "You dropped:\n";
-            Set<String> keys = droppedItems.keySet();
-
-            for (String key : keys) {
-                //Below code changes name formats from "IRON_AXE" to "Iron Axe".
-                //itemName = itemName.replace("_", " ");
-//              //itemName = WordUtils.capitalizeFully(itemName);
-                String temp = key.replace("_", " ");
-                temp = WordUtils.capitalizeFully(temp);
-                mes += temp + " x" + droppedItems.get(key) + ", ";
-            }
-            eventPlayer.sendMessage(mes);
-
-
-        }
+//
+//
+//
+//            String mes = "You dropped:\n";
+//            Set<String> keys = droppedItems.keySet();
+//
+//            for (String key : keys) {
+//                //Below code changes name formats from "IRON_AXE" to "Iron Axe".
+//                String temp = key.replace("_", " ");
+//                temp = WordUtils.capitalizeFully(temp);
+//                mes += temp + " x" + droppedItems.get(key) + ", ";
+//            }
+//            eventPlayer.sendMessage(mes);
+//
+//
+//        }
     }
 
-    private Plugin getPlugin() {
-        return this;
+    static Plugin getPlugin() {
+        return Bukkit.getServer().getPluginManager().getPlugin("ItemDeath");
     }
 
     private JavaPlugin getJavaPlugin() {
@@ -154,6 +148,19 @@ public final class  ItemDeath extends JavaPlugin implements Listener {
             totalXP = (4.5 * (level * level)) - (162.5 * level) + 2220;
         }
         return totalXP;
+    }
+
+    private void setupConfigs(){
+        config.addDefault("percent-items-to-drop", "25");
+        config.addDefault("log-death-loot", "true");
+        config.addDefault("honor-vanish-curse", "true");
+        config.options().copyDefaults(true);
+        saveConfig();
+
+    }
+
+    public static String colorFormat(String msg){
+        return ChatColor.translateAlternateColorCodes('&',msg);
     }
 }
 
--- src/main/java/net/gguppy/itemdeath/SetPlayerDataRunnable.java
+++ src/main/java/net/gguppy/itemdeath/SetPlayerDataRunnable.java
@@ -1,35 +1,118 @@
 package net.gguppy.itemdeath;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.apache.commons.text.WordUtils;
 import org.bukkit.Location;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
 import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SetPlayerDataRunnable extends BukkitRunnable {
     private Player eventPlayer;
-    private ItemStack[] contents;
-    private int randomExpDrop;
     private Location deathLoc;
+    private int itemDropPercent;
+    private boolean honorVanishCurse;
+    private boolean logDeathLoot;
 
-    public SetPlayerDataRunnable(Player eventPlayer, ItemStack[] contents, int randomExpDrop, Location deathLoc) {
-        this.randomExpDrop = randomExpDrop;
+    public SetPlayerDataRunnable(Player eventPlayer, Location deathLoc, int itemDropPercent, boolean honorVanishCurse, boolean logDeathLoot) {
         this.eventPlayer = eventPlayer;
-        this.contents = contents;
         this.deathLoc = deathLoc;
+        this.itemDropPercent = itemDropPercent;
+        this.honorVanishCurse = honorVanishCurse;
+        this.logDeathLoot = logDeathLoot;
     }
     @Override
     public void run() {
+
+
+        //Player inventory
+        ItemStack[] contents = eventPlayer.getInventory().getContents();
+        //A list to be filled with the indexes of items that aren't air.
+        ArrayList<Integer> nonAirItemIndexes = new ArrayList<>();
+        for(int i = 0;  i < contents.length; i++){
+            if(contents[i] != null){
+                nonAirItemIndexes.add(i);
+                //eventPlayer.sendMessage("Item slot " + contents[i] + " at slot " + i + " is not air." );
+            }
+        }
+
+        double temp = nonAirItemIndexes.size() * ((double)itemDropPercent / 100);
+        //eventPlayer.sendMessage(String.valueOf((nonAirItemIndexes.size() * ((double)itemDropPercent / 100))));
+        int dropCount = (int) Math.round(temp);
+        if(dropCount == 0) dropCount = 1;
+        if(dropCount > nonAirItemIndexes.size()) dropCount = nonAirItemIndexes.size();
+
+        //Message to be sent to player containing what they dropped
+        StringBuilder sb = new StringBuilder();
+        boolean droppedAnItem = false;
+        sb.append(ItemDeath.colorFormat("&8[&c&l!&8] &cYou dropped: &4"));
+        for(int i = 0; i < dropCount; i++){
+            int randomItemSlot = ThreadLocalRandom.current().nextInt(0, nonAirItemIndexes.size());
+            ItemStack itemToDrop = contents[nonAirItemIndexes.get(randomItemSlot)];
+            if(deathLoc.getWorld() != null){
+                //eventPlayer.sendMessage("Trying to drop " + contents[nonAirItemIndexes.get(randomItemSlot)] + " at slot " + nonAirItemIndexes.get(randomItemSlot) + "." );
+                if(!itemToDrop.containsEnchantment(Enchantment.VANISHING_CURSE)){
+                    deathLoc.getWorld().dropItem(deathLoc, itemToDrop);
+                } else {
+                    if(!honorVanishCurse) deathLoc.getWorld().dropItem(deathLoc, itemToDrop);
+                }
+                contents[nonAirItemIndexes.get(randomItemSlot)] = null;
+                String itemName = "";
+
+                //No custom item name
+                if(!itemToDrop.getItemMeta().hasDisplayName()){
+                    itemName = WordUtils.capitalizeFully(itemToDrop.getType().toString().replaceAll("_", " "));
+                    droppedAnItem = true;
+
+                }
+                //Custom Item name
+                else {
+                    itemName = itemToDrop.getItemMeta().getDisplayName();
+                    droppedAnItem = true;
+                }
+                sb.append(itemName).append(" x").append(itemToDrop.getAmount()).append(ItemDeath.colorFormat("&8,&4 "));
+                nonAirItemIndexes.remove(randomItemSlot);
+            } else {
+                ItemDeath.getPlugin().getLogger().severe("Could not find location to drop items. This shouldn't happen.");
+            }
+        }
+        if(droppedAnItem){
+            sb = new StringBuilder(sb.toString().substring(0, sb.length() - 4));
+            eventPlayer.sendMessage(sb.toString());
+            if(logDeathLoot) {
+                ItemDeath.getPlugin().getLogger().info(eventPlayer.getDisplayName() +"'s death loot: " + sb.substring(13, sb.length()));
+            }
+        }
         eventPlayer.getInventory().setContents(contents);
 
 
 
     }
 
+    private double levelToExp(double level) {
+        //Different formulas for levels 0-16, 17-31, and 32+;
+        double totalXP = 0;
+        //Level 0-16
+        if (level < 17) {
+            totalXP = (level * level) + (6 * level);
+            //Level 17-31
+        } else if (level < 32) {
+            totalXP = (2.5 * (level * level)) - (40.5 * level) + 360;
+            //Level 32+
+        } else {
+            totalXP = (4.5 * (level * level)) - (162.5 * level) + 2220;
+        }
+        return totalXP;
+    }
 
 
 
